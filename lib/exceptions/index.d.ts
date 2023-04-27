ce0755402bd3b14b854df130758e089e914f8153
adwemine <108937310+adwemine@users.noreply.github.com>
2023-04-27 09:17:55 -0400

    Regenerate lib

---
+++ lib/exceptions/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./rpc-exceptions";
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/exceptions/rpc-exceptions.d.ts
@@ -0,0 +1,6 @@
+export declare class RpcException extends Error {
+    status: number;
+    message: string;
+    constructor(status: number, message: string);
+}
+//# sourceMappingURL=rpc-exceptions.d.ts.map
