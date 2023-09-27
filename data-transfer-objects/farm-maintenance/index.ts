3e215d39c026d586a5db3d7ad19abd665bc39e8f
adwemine <108937310+adwemine@users.noreply.github.com>
2023-09-27 16:59:40 -0400

    Add missing export for MinerError

--- data-transfer-objects/farm-maintenance/index.ts
+++ data-transfer-objects/farm-maintenance/index.ts
@@ -3,6 +3,7 @@ export * from "./customer.dto";
 export * from "./facility-info.dto";
 export * from "./inventory-item.dto";
 export * from "./miner.dto";
+export * from "./miner-error.dto";
 export * from "./pool.dto";
 export * from "./supplier-quote.dto";
 export * from "./uptime-tick.dto";
