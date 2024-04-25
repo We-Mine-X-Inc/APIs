839224a8d761296b9c186e58f3c2b4fd34e34896
adwemine <108937310+adwemine@users.noreply.github.com>
2024-04-25 08:28:14 -0400

    cleanup unused model and apis

--- lib/business-logic-interfaces/farm-maintenance/assertions/inventory-item.d.ts
+++
@@ -1,3 +0,0 @@
-import { InventoryItem } from "../inventory-item.interface";
-export declare function assertInventoryItem(value: InventoryItem): asserts value is InventoryItem;
-//# sourceMappingURL=inventory-item.d.ts.map
